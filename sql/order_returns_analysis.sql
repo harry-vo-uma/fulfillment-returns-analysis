182c17ef1dab8e3acef2da64eabf05c4db88bc2a
harry-vo-uma <vhoangphuc@umass.edu>
2025-07-31 23:21:58 -0400

    Update order_returns_analysis.sql

--- sql/order_returns_analysis.sql
+++ sql/order_returns_analysis.sql
@@ -1,1 +1,19 @@
 
+WITH sku_stats AS (
+    SELECT
+        sku,
+        COUNT(*)               AS total_orders,
+        SUM(is_return)         AS total_returns,
+        ROUND(
+            100.0 * SUM(is_return) / NULLIF(COUNT(*), 0),
+            2
+        )                      AS return_rate_pct
+    FROM {{ returns_table }}
+    GROUP BY sku
+)
+
+SELECT *
+FROM sku_stats
+ORDER BY return_rate_pct DESC
+LIMIT 10;
+
